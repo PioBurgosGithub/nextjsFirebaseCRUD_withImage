9aed6d890746875c533a72d00c8628e060ffd059
Pio Lawrence Burgos <piolawrence.burgos@wvsu.edu.ph>
2024-04-24 14:39:27 +0800

    creating .env file

--- firebase.js
+++ firebase.js
@@ -9,12 +9,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBsVM_1tf-o3Qc3UADPwwXlNZrr7W1ePkM",
-  authDomain: "fir-crud-d9b94.firebaseapp.com",
-  projectId: "fir-crud-d9b94",
-  storageBucket: "fir-crud-d9b94.appspot.com",
-  messagingSenderId: "28733067773",
-  appId: "1:28733067773:web:f1a7b120953d43a3e68994"
+  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
